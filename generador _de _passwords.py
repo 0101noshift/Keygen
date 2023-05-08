2820fe054e7581a70b321509910f2a047fc73745
0101noshift <nahu_3_6@hotmail.com>
2023-05-08 04:09:13 -0300

    Update generador _de _passwords.py

--- generador _de _passwords.py
+++ generador _de _passwords.py
@@ -2,17 +2,20 @@ from tkinter import *
 import random
 import string
 
-def generar_passw():
-    longitud = 8
-    caracteres = string.ascii_letters + string.digits + string.punctuation
-    passw = ''.join(random.choice(caracteres) for i in range(longitud))
-    return passw
-
-def actualizar_passw():
-    passw = generar_passw()
-    etiqueta_passw.config(text=passw)
-
 root = Tk()
+
+# def generar_passw():
+#     global longitud
+#     caracteres = string.ascii_letters + string.digits + string.punctuation
+#     passw = ''.join(random.choice(caracteres) for i in range(longitud))
+#     return passw
+
+# def actualizar_passw():
+#     passw = generar_passw()
+#     etiqueta_passw.config(text=passw)
+
+longitud = Spinbox(from_=4, to=25, increment=1, state="readonly").pack()
+
 
 root.title("Generador de passws")
 root.geometry("200x200")
